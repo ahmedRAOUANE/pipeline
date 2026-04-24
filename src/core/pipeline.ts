bc88ae857d4762bc88c656a7fa83340a24de151d
ahmed RAOUANE <ahmedraouane30@gmail.com>
2026-04-24 16:42:25 +0100

    feat: enhance plugin system to support function plugins and improve error handling

--- src/core/pipeline.ts
+++ src/core/pipeline.ts
@@ -1,35 +1,46 @@
 import { executePipeline } from "./executor";
 import { PipelineConfig, PipelineContext, PipelineFile } from "./types";
-import { PipelinePlugin } from "./plugin";
+import { PipelinePlugin, PipelinePluginSetup, isPipelinePlugin } from "./plugin";
 import { PipelineBuilder } from "./builder";
+import { PluginMeta } from "./plugin-meta";
 
 export function createPipeline(config: PipelineConfig) {
     const builder = new PipelineBuilder(config);
 
     return {
-        use(plugin: PipelinePlugin) {
-            plugin.setup(builder);
-            builder.registerPlugin({
-                name: plugin.name ?? "unknown",
-                version: plugin.version as unknown as string,
-            });
+        use(plugin: PipelinePlugin | PipelinePluginSetup) {
+            let setup: PipelinePluginSetup;
+            let name: string = "anonymous-plugin";
+            let version: string | undefined;
+
+            if (isPipelinePlugin(plugin)) {
+                setup = plugin.setup;
+                name = plugin.name;
+                version = plugin.version;
+            } else if (typeof plugin === 'function') {
+                setup = plugin;
+                name = plugin.name || "anonymous-plugin";
+            } else {
+                throw new Error("Plugin must be an object with setup() or a function");
+            }
+
+            setup(builder);
+            builder.registerPlugin({ name, version } as unknown as PluginMeta);
             return this;
         },
-
         async process(file: PipelineFile) {
             const ctx: PipelineContext = {
                 file,
                 metadata: {},
                 meta: builder.meta
             };
-
             return executePipeline({
                 ctx,
                 storage: builder.storage,
-                ...(builder.validators && { validators: builder.validators }),
-                ...(builder.processors && { processors: builder.processors }),
-                ...(builder.hooks && { hooks: builder.hooks }),
+                validators: builder.validators,
+                processors: builder.processors,
+                hooks: builder.hooks,
             });
-        },
+        }
     };
 }
--- src/core/plugin.ts
+++ src/core/plugin.ts
@@ -1,7 +1,13 @@
 import { PipelineBuilder } from "./builder";
 
+export type PipelinePluginSetup = (builder: PipelineBuilder) => void;
+
 export type PipelinePlugin = {
     name: string,
     version?: string,
-    setup: (builder: PipelineBuilder) => void
+    setup: PipelinePluginSetup
 };
+
+export function isPipelinePlugin(obj: any): obj is PipelinePlugin {
+    return obj && typeof obj === 'object' && 'setup' in obj && typeof obj.setup === 'function';
+}
